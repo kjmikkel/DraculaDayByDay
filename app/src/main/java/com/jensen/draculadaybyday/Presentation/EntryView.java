66110d41b065f7a48d2030ec222460487d631cf6
Mikkel Kjær Jensen <kjmikkel@gmail.com>
2016-12-25 14:02:51 +0100

    Added a very bare bones system for adding markup to the text. In this first version it is date, location, and the entry text. This system may be extended in the future, but it is unlikely.

--- app/src/main/java/com/jensen/draculadaybyday/Presentation/EntryView.java
+++ app/src/main/java/com/jensen/draculadaybyday/Presentation/EntryView.java
@@ -3,15 +3,23 @@ package com.jensen.draculadaybyday.Presentation;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Typeface;
+import android.text.Layout;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
+import android.text.style.AlignmentSpan;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.widget.TextView;
 
+import com.jensen.draculadaybyday.Primitives.Tuple;
+
+import java.util.regex.Pattern;
+
 public class EntryView extends TextView {
 
     private static final int SIZE_UNIT = TypedValue.COMPLEX_UNIT_DIP;
+
+    private static final Pattern date = Pattern.compile("[0-9]+");
 
     public EntryView(Context context) {
         super(context);
@@ -35,19 +43,105 @@ public class EntryView extends TextView {
 
     private void setTextWithoutInitial(String text, FontEnum fontEnum, float fontSize) {
         setTextSize(SIZE_UNIT,fontSize);
-        setTypeface(getMainBodyType(fontEnum));
-        setText(text);
+
+        String finalText = "";
+
+        String dateStr = null;
+        Tuple<Integer, Integer> dateInterval = findDatePart(text);
+        if (dateInterval.fst != -1 && dateInterval.snd != -1) {
+            dateStr = text.substring(dateInterval.fst, dateInterval.snd) + "\n";
+            finalText += dateStr;
+        }
+
+        String locationStr = null;
+        Tuple<Integer, Integer> locationInterval = findLocation(text);
+        if (locationInterval.fst != -1 && locationInterval.snd != -1) {
+            locationStr = text.substring(locationInterval.fst, locationInterval.snd) + "\n\n";
+            finalText += locationStr;
+        }
+
+        Tuple<Integer, Integer> entryInterval = findEntry(text);
+        if (entryInterval.fst != -1 && entryInterval.snd != -1) {
+            finalText  += text.substring(entryInterval.fst, entryInterval.snd);
+        }
+
+        int currentIndex = 0;
+        SpannableStringBuilder SS = new SpannableStringBuilder(finalText);
+        if (dateInterval != null) {
+            SS.setSpan(new AlignmentSpan.Standard(Layout.Alignment.ALIGN_CENTER), currentIndex, currentIndex + dateStr.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+            currentIndex += dateStr.length();
+        }
+        if (locationInterval != null) {
+            SS.setSpan(new AlignmentSpan.Standard(Layout.Alignment.ALIGN_CENTER), currentIndex, currentIndex + locationStr.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+        }
+        SS.setSpan(new CustomTypefaceSpan("", getMainBodyType(fontEnum)), 0, finalText.length() - 1, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+
+        setText(SS);
     }
 
     private void setTextWithInitial(String text, InitialEnum initialFont, FontEnum basicFont, float fontSize) {
         setTextSize(SIZE_UNIT, fontSize);
 
-        SpannableStringBuilder SS = new SpannableStringBuilder(text);
+        String finalText = "";
 
-        // SS.setSpan(new CustomTypefaceSpan("", getInitialType(initialFont)), 0, 10, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
-        SS.setSpan(new CustomTypefaceSpan("", getMainBodyType(basicFont)), 1, text.length() - 1, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+        String dateStr = null;
+        Tuple<Integer, Integer> dateInterval = findDatePart(text);
+        if (dateInterval.fst != -1 && dateInterval.snd != -1) {
+            dateStr = text.substring(dateInterval.fst, dateInterval.snd) + "\n";
+            finalText += dateStr;
+        }
+
+        String locationStr = null;
+        Tuple<Integer, Integer> locationInterval = findLocation(text);
+        if (locationInterval.fst != -1 && locationInterval.snd != -1) {
+            locationStr = text.substring(locationInterval.fst, locationInterval.snd) + "\n\n";
+            finalText += locationStr;
+        }
+
+        Tuple<Integer, Integer> entryInterval = findEntry(text);
+        if (entryInterval.fst != -1 && entryInterval.snd != -1) {
+            finalText  += text.substring(entryInterval.fst, entryInterval.snd);
+        }
+
+        int currentIndex = 0;
+        SpannableStringBuilder SS = new SpannableStringBuilder(finalText);
+        if (dateInterval != null) {
+            SS.setSpan(new AlignmentSpan.Standard(Layout.Alignment.ALIGN_CENTER), currentIndex, currentIndex + dateStr.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+            SS.setSpan(new CustomTypefaceSpan("", getMainBodyType(basicFont)),    currentIndex, currentIndex + dateStr.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+            currentIndex += dateStr.length();
+        }
+        if (locationInterval != null) {
+            SS.setSpan(new AlignmentSpan.Standard(Layout.Alignment.ALIGN_CENTER), currentIndex, currentIndex + locationStr.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+            SS.setSpan(new CustomTypefaceSpan("", getMainBodyType(basicFont)),    currentIndex, currentIndex + locationStr.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+            currentIndex += locationStr.length();
+        }
+
+        SS.setSpan(new CustomTypefaceSpan("", getInitialType(initialFont)), currentIndex, currentIndex + 1, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+        SS.setSpan(new CustomTypefaceSpan("", getMainBodyType(basicFont)), currentIndex + 1, finalText.length() - 1, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
 
         setText(SS);
+    }
+
+    private Tuple<Integer, Integer> findDatePart(String text) {
+        return parseText("d", text);
+    }
+
+    private Tuple<Integer, Integer> findLocation(String text) {
+        return parseText("l", text);
+    }
+
+    private Tuple<Integer, Integer> findEntry(String text) {
+        return parseText("e", text);
+    }
+
+    private Tuple<Integer, Integer> parseText(String indicator, String text) {
+        String start = String.format("[%s]", indicator);
+        String end = String.format("[/%s]", indicator);
+
+        int startIndex = text.indexOf(start) + start.length();
+        int endIndex = text.indexOf(end);
+
+        return new Tuple<>(startIndex, endIndex);
     }
 
     private Typeface makeFont(String fontName) {
