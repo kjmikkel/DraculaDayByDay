8d1f0b4d349bb4e8fe6a2b934721486f78977caa
Mikkel Kjær Jensen <kjmikkel@gmail.com>
2017-07-10 22:36:49 +0200

    Fixed spelling error

--- app/src/main/java/com/jensen/draculadaybyday/preferences/TimePreference.java
+++ app/src/main/java/com/jensen/draculadaybyday/preferences/TimePreference.java
@@ -13,8 +13,8 @@ class TimePreference extends DialogPreference {
     private int lastMinute = 0;
     private TimePicker picker = null;
 
-    public TimePreference(Context ctxt, AttributeSet attrs) {
-        super(ctxt, attrs);
+    public TimePreference(Context contxt, AttributeSet attrs) {
+        super(contxt, attrs);
 
         setPositiveButtonText("Set");
         setNegativeButtonText("Cancel");
