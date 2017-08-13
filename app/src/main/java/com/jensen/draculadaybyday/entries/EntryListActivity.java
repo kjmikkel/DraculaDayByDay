acb22445d3f68e9998715474c81286687fba6ec2
Mikkel Kjær Jensen <kjmikkel@gmail.com>
2017-08-13 14:31:15 +0200

    Removed some cruft

--- app/src/main/java/com/jensen/draculadaybyday/entries/EntryListActivity.java
+++ app/src/main/java/com/jensen/draculadaybyday/entries/EntryListActivity.java
@@ -603,7 +603,6 @@ public class EntryListActivity extends AppCompatActivity {
         public void onBindViewHolder(@NonNull final EntryViewHolder holder, int position) {
             // Set the views
             holder.entry = mValues.get(position);
-            // holder.mNewsType.getLayoutParams().height = holder.mNewsType.getWidth();
             holder.setViews();
 
             //region View on click
